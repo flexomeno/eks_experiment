d8c76c02fdd955d2780674d06733467ac1b9974e
flexomeno <wilrengui@gmail.com>
2023-04-19 23:10:26 -0500

    Fixed eks1 outputs

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,5 +1,13 @@
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.main.name
+}
+
 data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.main.name
+
+  depends_on = [
+    aws_eks_cluster.main,
+  ]
 }
 
 output "kubeconfig" {
