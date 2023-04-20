e3fa7f181d71d095bc3a796563a77f9313ae81d7
flexomeno <wilrengui@gmail.com>
2023-04-19 23:06:00 -0500

    Fixed vpc outputs

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,3 +5,27 @@ resource "aws_vpc" "main" {
     Name = "eks-vpc"
   }
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+
+  availability_zone = "us-east-1c"
+}
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,11 @@
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id,
+    aws_subnet.public_subnet_3.id,
+  ]
+}
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
